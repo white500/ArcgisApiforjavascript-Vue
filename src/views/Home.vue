6af79db41f351c40b8275001ad3f541cc81ce5b8
white500 <964142664@qq.com>
2020-10-18 23:50:57 +0800

    加载谷歌webTileLayer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-18 21:31:53
+ * @LastEditTime: 2020-10-18 23:44:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \arcgis-test-demo\src\views\Home.vue
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="mapView"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { loadModules } from 'esri-loader'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'map-view',
+  data () {
+    return {
+      view: null
+    }
+  },
+  components: {},
+  mounted () {
+    this.creatMap()
+  },
+  methods: {
+    creatMap () {
+      loadModules([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/Basemap",
+        "esri/layers/WebTileLayer"
+      ], { css: true })
+      .then(([
+        Map,
+        MapView,
+        Basemap,
+        WebTileLayer,
+      ]) => {
+        const wtl = new WebTileLayer({
+          urlTemplate: 'http://mt0.google.cn/vt/lyrs=t@132,r@280000000&hl=zh-CN&gl=cn&src=app&x={col}&y={row}&z={level}&s=Gali'
+        });
+        var basemap = new Basemap({
+          baseLayers: [
+           wtl
+          ],
+          referenceLayers: [
+            wtl
+          ],
+        });
+
+        // add the new instance of the custom tile layer the map
+        var map = new Map({
+          basemap: basemap
+        });
+        
+        this.view = new MapView({
+          container: this.$el,
+          map: map,
+          center: [116.403119, 39.914935],
+          zoom: 12
+        })
+      })
+    }
+  },
+  destroyed () {
+    if (this.view) {
+      // destroy the map view
+      this.view.destroyed()
+    }
   }
 }
 </script>
+<style>
+#mapView {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 80vh;
+  border: 1px solid black;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,47 @@
+/*
+ * @Author: zhangyang
+ * @Date: 2020-10-12 13:35:39
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-10-18 22:31:10
+ * @Description: 项目配置文件
+ */
+const resolves = dir => require('path').join(__dirname, dir)
+module.exports = {
+    publicPath: './',
+    outputDir: 'dist', // 打包输出目录
+    assetsDir: 'static',
+    filenameHashing: true,
+    lintOnSave: false,
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolves('src'))
+            .set('@u', resolves('src/utils'))
+            .set('@center', resolves('src/center'))
+            .set('@c', resolves('src/components'))
+            .set('@api', resolves('src/api'))
+            .set('@v', resolves('src/views'))
+    },
+    configureWebpack: config => {
+        // 调试JS
+        config.devtool = 'source-map'
+    },
+    css: {
+        // 查看CSS属于哪个css文件
+        sourceMap: true
+    },
+//     devServer: {
+//         // public: '0.0.0.0:8080',
+//         // port: 8080,
+//         https: false,
+//         open: false,
+//         proxy: {
+//             '/api': {
+//                 target: process.env.VUE_APP_BASE_URL,
+//                 ws: true,
+//                 secure: false,
+//                 changeOrigin: true,
+//                 pathReWrite: { '^/api': '' }
+//             }
+//         }
+//     }
+}
